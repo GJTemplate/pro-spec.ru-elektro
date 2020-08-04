35a68b9b85afe14e9ba3bea5063e40b82d7290eb
oleg <sad.net79@gmail.com>
2020-08-04 12:30:49 +0300

    README Edit

---
+++ html/layouts/heder/product/get_discount.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     ${NAMESPACE}
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
+
+
+
+?>
+<!-- OLD HTML : 
+<div style="" abbr="" class="jcetooltip" title="Заполните форму заявки и наши менеджеры свяжутся с вами.">
+    <input type="button" class="b1c" value="Нашли дешевле? Снизим цену!">
+</div>
+-->
+
+<div style="" abbr class="l-get-discount">
+    <span class="l-get-discount-btn"></span>
+</div>
+
+
+
+
